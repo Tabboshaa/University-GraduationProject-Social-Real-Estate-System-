0cd427ccfabc7513eadca099b8a8e2aa82c303fe
Abdelaziz <20173089@fue.edu.eg>
2020-12-04 16:26:23 +0200

    testtestest

--- realEstate/resources/views/website/backend/database pages/Main_Types_Show.blade.php
+++ realEstate/resources/views/website/backend/database pages/Main_Types_Show.blade.php
@@ -6,7 +6,6 @@
 <table id="datatable" class="table table-striped table-bordered dataTable no-footer" style="width: 100%;" role="grid" aria-describedby="datatable_info">
     <thead>
         <tr>
-            
             <th>Type Name</th>
             <th>Select all <input type="checkbox" id="selectAll" name="selectAll">  <input type="submit" value="Delete Selected" class="btn btn-secondary"></th>
             <th></th>
@@ -23,20 +22,18 @@
     </thead>
     <tbody>
         <!-- EL FOREARCH HNA -->
-
-
         @foreach($main_type as $main_type)
 
         <tr>
             <td>{{$main_type->Main_Type_Name}}</td>
             <td><input type="checkbox" name="mainType[]" value="{{$main_type->Main_Type_Id}}"></td>
-             
+
          <td><a href="javascript:void(0)" onclick="setMainTypeIdName('{{$main_type->Main_Type_Id}}','{{$main_type->Main_Type_Name}}')"><i class="fa fa-edit"> Edit</i></a></td>
                 </tr>
 
                 @endforeach
 
-                       
+
                 <!-- END OF FOREACH -->
     </tbody>
 </table>
@@ -55,11 +52,11 @@
                 <form id="EditSubTypeForm">
                     @csrf
                     <input type="hidden" name="id" id="id">
-          
+
                     <div class="form-group">
                         <label for="MainTypeName" >Main Type Name</label>
                         <input type="text" name="MainTypeName" id="MainTypeName" class="form-control">
-                        
+
                     </div>
                     <button  type="submit" class="btn btn-success">Edit</button>
                 </form>
@@ -81,7 +78,7 @@
             var id=$("#id").val();
             var MainTypeName=$("#MainTypeName").val();
             var _token= $("input[name=_token]").val();
-            
+
             $.ajax({
                 url:"{{route('Maintype.update')}}",
                 Type:"PUT",
