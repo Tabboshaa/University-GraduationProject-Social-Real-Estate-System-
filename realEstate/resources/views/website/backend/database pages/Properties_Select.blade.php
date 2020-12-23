1e6ecbadfe02f1fb0ad466e7ee9e5781fc021745
omnia-fathy <61743696+omnia-fathy@users.noreply.github.com>
2020-12-23 11:55:15 +0200

    Om

--- realEstate/resources/views/website/backend/database pages/Properties_Select.blade.php
+++ realEstate/resources/views/website/backend/database pages/Properties_Select.blade.php
@@ -5,46 +5,35 @@
 
 <form method="Post" action="{{url('/')}}" enctype="multipart/form-data">
     @csrf
-    @foreach($images as $image)
-        @foreach($property as $p)
-        
-            <div class="col-sm-3">
-                
-                <table id="cateagories">
-                    <body>
-                        
-                        <tr>
-                            <td>
-                                
-                                <a href="javascript:void(0)" id="details" onclick="AddDetail('{{$p->Property_Id}}','{{$p->Property_Name}}')"> 
-                                   
-                                    <img  src="{{ asset('Images/' . $image->getFilename()) }}" name="PropertyImage">
-                                   
-                                    <label for="Sub_Type_Property" class="col-md-2 col-form-label text-md-right">{{ __($p->Property_Name) }}</label> 
-                                </a>
-                                
-                            </td>
-                          
-                        </tr>
-                        
-                    </body>
-                </table>
-                
 
-            </div>
-        @endforeach
-        @endforeach
+    @foreach($property as $p)
 
-        <tr>
-        <!-- send item id to be shown in show item page -->
-            <td>
-                <button type="submit" class="btn btn-primary">
-                    {{ __('Next') }}
-                </button>
+    <div class="col-sm-3">
 
-            </td>
-        </tr>
+        <table id="cateagories">
+            <tr>
+                <td>
+                    <div class="fancy">
+                    <a href="javascript:void(0)" id="details" onclick="AddDetail('{{$p->Property_Id}}','{{$p->Property_Name}}')">
 
+                        <label for="Sub_Type_Property" class="col-md-2 col-form-label text-md-right">{{ __($p->Property_Name) }}</label>
+                    </a>
+                    </div>
+                </td>
+
+            </tr>
+
+
+    </div>
+    @endforeach
+    <tr>
+        <td>
+            <button type="submit" class="btn btn-primary">
+                {{ __('Next') }}
+            </button>
+        </td>
+    </tr>
+    </table>
 </form>
 
 <div class="modal fade" id="EditMainTypeModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -59,7 +48,7 @@
             <div class="modal-body">
                 <form id="data_form">
                     @csrf
-                   
+
                 </form>
 
             </div>
@@ -68,88 +57,91 @@
 </div>
 
 <script>
+    var myPix = new Array("Images/1.jpeg", "Images/2.jpeg", "Images/3.jpeg", "Images/4.jpeg", "Images/5.jpeg", "Images/6.jpeg");
+    $(document).ready(function() {
 
-window.onload = choosePic;
-
-var myPix = new Array("Images/1.jpeg","Images/2.jpeg","Images/3.jpeg","Images/4.jpeg","Images/5.jpeg","Images/6.jpeg");
-
-function choosePic() {
-
-     var randomNum = Math.floor(Math.random() * myPix.length);
-     document.getElementsByName("PropertyImage").src = myPix[randomNum];
-}
+        function choosePic() {
+            $('img[name="PropertyImage[]"]').each(function() {
+                var randomNum = Math.floor(Math.random() * myPix.length);
+                this.src = " {{ asset('Images/1.jpeg')}}";
+            });
+        }
+    });
 
     function AddDetail(id, name) {
 
-        $("#exampleModalLabel").html(name); 
-        var Form='' ; 
-        $.ajax({ 
-            url: "{{route('propertyDetail.find')}}" , 
-            Type: "PUT" , 
-            data: { id: id }, 
-            success: 
-            function(data) 
-            { 
-                console.log('success'); 
-                $("#EditMainTypeModal").modal("toggle"); 
-                
-                 Object.values(data).forEach(val=> {
+        $("#exampleModalLabel").html(name);
+        var Form = '';
+        $.ajax({
+            url: "{{route('propertyDetail.find')}}",
+            Type: "PUT",
+            data: {
+                id: id
+            },
+            success: function(data) {
+                console.log('success');
+                $("#EditMainTypeModal").modal("toggle");
+
+                Object.values(data).forEach(val => {
 
                     Form += ' <div class="form-group row"> ' +
                         '<label for="' + val['Property_Detail_Id'] + '" class="col-md-2 col-form-label text-md-right">' + val['Detail_Name'] + '</label>' +
                         '<div class="col-md-5">' +
-                            '<input type="' + val['datatype'] + '" id="' + val['Property_Detail_Id'] + '" name="DetailItem[]" class="form-control" required autocomplete="DetailName">' +
-                            '</div>'+
-                    '</div>';
-                    });
-                    if (Form == '')
+                        '<input type="' + val['datatype'] + '" id="' + val['Property_Detail_Id'] + '" name="DetailItem[]" class="form-control" required autocomplete="DetailName">' +
+                        '</div>' +
+                        '</div>';
+                });
+                if (Form == '')
                     Form = 'No Property Details';
-                    else
+                else
                     Form += ' <div class="form-group row mb-0">' +
-                        '<div class="col-md-2 offset-md-2">' +
-                            ' <button type="submit" class="btn btn-primary">' +
-                                ' {{ __("Add") }}';
-                                '</button>';
+                    '<div class="col-md-2 offset-md-2">' +
+                    ' <button type="submit" class="btn btn-primary">' +
+                    ' {{ __("Add") }}';
+                '</button>';
 
 
-                            $('#data_form').html(Form);
-                            // var FormTag= document.getElementById('data_form').innerHTML()=Form;
-                            },
-                            error: function() {
-                            console.log('Error');
-                            }
+                $('#data_form').html(Form);
+                // var FormTag= document.getElementById('data_form').innerHTML()=Form;
+            },
+            error: function() {
+                console.log('Error');
+            }
 
-                            });
+        });
 
-                            }
+    }
 
-    $('#data_form').submit(function (){
-                            var data=[];
+    $('#data_form').submit(function() {
+        var data = [];
 
-                            //3iza ageeb kol el inputs b get element by name
-                            //w b3deen 3iza 27ot el inputs value&id f array
-                            $('input[name="DetailItem[]"]').each(function() {
-                            data.push({id: this.id, value: this.value});
-                            });
-                            var _token= $("input[name=_token]").val();
-                            //w b3den 3iza 2b3t el array le el controller
-                            $.ajax({
-                            type: "post",
-                            url: "{{ route('details_submit')}}",
-                            data: {
-                            data: data,
-                            _token:_token
-                            },
-                            success: function() {
-                                //hna 3iza anady 3la created succefully
-                              console.log('Success');
-                            },
-                            error: function() {
-                                // hna anady 3la not created w kdaho
-                            console.log('Error');
-                            }
-                            });
+        //3iza ageeb kol el inputs b get element by name
+        //w b3deen 3iza 27ot el inputs value&id f array
+        $('input[name="DetailItem[]"]').each(function() {
+            data.push({
+                id: this.id,
+                value: this.value
+            });
+        });
+        var _token = $("input[name=_token]").val();
+        //w b3den 3iza 2b3t el array le el controller
+        $.ajax({
+            type: "post",
+            url: "{{ route('details_submit')}}",
+            data: {
+                data: data,
+                _token: _token
+            },
+            success: function() {
+                //hna 3iza anady 3la created succefully
+                console.log('Success');
+            },
+            error: function() {
+                // hna anady 3la not created w kdaho
+                console.log('Error');
+            }
+        });
 
-                            });
-                            </script>
-                            @endsection
+    });
+</script>
+@endsection
