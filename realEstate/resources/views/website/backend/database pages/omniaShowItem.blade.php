18894ee11fb130493559532e4b5702fe97f76f1c
shaimaa473 <75483443+shaimaa473@users.noreply.github.com>
2020-12-30 17:18:40 +0200

    shaimaa style

--- realEstate/resources/views/website/backend/database pages/omniaShowItem.blade.php
+++ realEstate/resources/views/website/backend/database pages/omniaShowItem.blade.php
@@ -1,34 +1,48 @@
 @extends('website.backend.database pages.Item')
 @section('Item_Main_Type_table')
 
-<table id="datatable" class="table table-bordered dataTable no-footer" style="width: 100%;" role="grid" aria-describedby="datatable_info">
+<link href="{{asset('css/ShowItem.css')}}" rel="stylesheet" type="text/css" />
+
+
+<div class="C">
+    
+    <h2>{{$user->First_Name}} {{$user->Middle_Name}} {{$user->Last_Name}} Items </h2> 
+    
+</div>
+
+<table id="datatable" class="table  pro  dataTable no-footer" style="width: 100%;" role="grid" aria-describedby="datatable_info">
     <thead>
         <tr>
-            <th>User</th>
-            <td>{{$user->First_Name}} {{$user->Middle_Name}} {{$user->Last_Name}} <br> {{$email->email}} <br> {{$phone_number->phone_number}}</td>
+            
+            <td class="th1">User</td>
+            <td class="td1"> 
+                 Name : {{$user->First_Name}} {{$user->Middle_Name}} {{$user->Last_Name}} 
+                <br>Email : {{$email->email}} 
+                <br>Phone Number :{{$phone_number->phone_number}}
+            </td>
         </tr>
     </thead>
     <tbody>
         <tr>
-            <th>Location</th>
-            <td>{{$Location->Country_Name}},{{$Location->State_Name}},{{$Location->City_Name}},{{$Location->Region_Name}},{{$Location->Street_Name}}</td>
+            <th class="th1">Location</th>
+            <td class="td1">{{$Location->Country_Name}},{{$Location->State_Name}},{{$Location->City_Name}},{{$Location->Region_Name}},{{$Location->Street_Name}}</td>
         </tr>
         <tr>
-            <td colspan="2" style="text-align:center">Details</td>
+            <td colspan="2" style="text-align:center" class="td1">Details</td>
         </tr>
         <tr>
             <form method="Post" action="{{ url('/delete_detail_item?_method=delete') }}" enctype="multipart/form-data">
                 @csrf
-                <table class="table table-bordered dataTable no-footer" style="width: 100%;" role="grid" aria-describedby="datatable_info">
+                <table class="table pro2 table-bordered dataTable no-footer" style="width: 100%;" role="grid" aria-describedby="datatable_info">
 
                     @foreach ($details as $property => $detail)
                     <tr>
-                        <td>
+                        <td class="th2">
                             <h4>{{$property}}</h4>
                         </td>
-                        <th></th>
-                        <th>Select all <input type="checkbox" id="selectAll" name="selectAll"> </a> <input type="submit" value="Delete Selected" class="btn btn-secondary"></th>
-                        <th></th>
+                        <th class="th2">value</th>
+                        <th class="th2">Select all <input type="checkbox" id="selectAll" name="selectAll"> </a> <input type="submit" value="Delete Selected" class="btn btn-secondary"></th>
+                        <th class="th2">Edit</th>
                         <!-- Java Script for select all function -->
                         <script>
                             document.getElementById('selectAll').onclick = function() {
@@ -42,7 +56,7 @@
                     </tr>
 
                     @foreach($detail as $detailValue)
-                    <tr>
+                    <tr  class="ha">
                         <td>
                             <h6>{{$detailValue->Detail_Name}}</h6>
                         </td>
@@ -50,7 +64,7 @@
                             <h6>{{$detailValue->DetailValue}}</h6>
                         </td>
                         <td><input type="checkbox" name="id[]" value="{{$detailValue->Detail_Id}}"></td>
-                        <td><a href="javascript:void(0)" onclick="setDetailIdName('{{$detailValue->Detail_Id}}','{{$detailValue->DetailValue}}')"><i class="fa fa-edit"> Edit</i></a></td>
+                        <td><a href="javascript:void(0)" onclick="setDetailIdName('{{$detailValue->Detail_Id}}','{{$detailValue->DetailValue}}')"><i class="fa fa-edit"></i></a></td>
 
                     </tr>
 
@@ -66,16 +80,16 @@
 </form>
 
 @if(!empty($subtypeid))
-<a href="{{url('/property_select/'.$item_id.'/'.$subtypeid.'')}}" class="btn btn-info"> Add More Details</a>
+<a href="{{url('/property_select/'.$item_id.'/'.$subtypeid.'')}}" id="btun1"class="btn btn-info "> Add More Details</a>
 @else 
-<a href="{{url('/addItemSteps/'.$item_id)}}" class="btn btn-info"> Add Details of item</a>
+<a href="{{url('/addItemSteps/'.$item_id)}}" class="btn btn-info"id="btun1" > Add Details of item</a>
 
 @endif
-<a href="{{url('/Details')}}" class="btn btn-info">Search for an Item</a>
-<a href="{{url('/Item')}}" class="btn btn-info"> Create Another Item</a>
+<a href="{{url('/Details')}}" class="btn btn-info" id="btun3">Search for an Item</a>
+<a href="{{url('/Item')}}" class="btn btn-info" id="btun2"> Create Another Item</a>
 <form method="Post" action="{{ url('/DelteItem/'.$item_id.'?_method=delete') }}" enctype="multipart/form-data">
                 @csrf
-<button type="submit" class="btn btn-danger"> Delete Item</button>
+<button type="submit" class="btn btn-danger" id="btun4"> Delete Item</button>
 </form>
 <div class="modal fade" id="EditDetailModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
@@ -92,10 +106,10 @@
                     <input type="hidden" name="id" id="id">
 
                     <div class="form-group">
-                        <label for="DetailName">Detail Value</label>
-                        <input type="text" name="DetailName" id="DetailName" class="form-control">
+                        <label for="DetailName"  style="font-size: 12pt">Detail Value</label>
+                        <input type="text" style="border-radius: 3pt" name="DetailName" id="DetailName" class="form-control">
                     </div>
-                    <button type="submit" class="btn btn-success">Edit</button>
+                    <button type="submit" class="btn btn-success" id="btun5">Edit</button>
                 </form>
 
             </div>
