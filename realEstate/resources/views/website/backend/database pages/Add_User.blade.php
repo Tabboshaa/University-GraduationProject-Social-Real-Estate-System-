232201af443464bd6434aea2978dd087bf2096d7
AlyZayed44 <61541819+AlyZayed44@users.noreply.github.com>
2021-01-01 13:17:50 +0200

    Ali

--- realEstate/resources/views/website/backend/database pages/Add_User.blade.php
+++ realEstate/resources/views/website/backend/database pages/Add_User.blade.php
@@ -172,9 +172,9 @@
 						</form>
 
 						<script>
-							
 							var currentTab = 0; // Current tab is set to be the first tab (0)
 							showTab(currentTab); // Display the current tab
+
 							function showTab(n) {
 							// This function will display the specified tab of the form ...
 							var x = document.getElementsByClassName("tab");
