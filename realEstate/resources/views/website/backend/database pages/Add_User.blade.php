42a074be3fe0abf881122acdb06a9737f17be488
shaimaa473 <75483443+shaimaa473@users.noreply.github.com>
2021-01-20 15:37:36 +0200

    v

--- realEstate/resources/views/website/backend/database pages/Add_User.blade.php
+++ realEstate/resources/views/website/backend/database pages/Add_User.blade.php
@@ -71,6 +71,10 @@
 									</script>
 								</div>
 							</div>
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 							<div class="item form-group">
 								<label class="col-form-label col-md-3 col-sm-3 label-align">Gender <span class="required">*  :</span></label>
 								<div class="col-md-6 col-sm-6 ">
