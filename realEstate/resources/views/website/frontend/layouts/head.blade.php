1f892a88b7f6e79a6b2cdeb1cadda6caec7cf148
omnia-fathy <61743696+omnia-fathy@users.noreply.github.com>
2021-05-04 11:32:00 +0200

    Update head.blade.php

--- realEstate/resources/views/website/frontend/layouts/head.blade.php
+++ realEstate/resources/views/website/frontend/layouts/head.blade.php
@@ -28,8 +28,6 @@
     <link href="{{asset('FrontEnd/css/font-awesome.min.css')}}" rel="stylesheet" type="text/css">
 	 <link rel="stylesheet" href="{{asset('FrontEnd/assets/owlcarousel/assets/owl.carousel.min.css')}}">
     <link rel="stylesheet" href="{{asset('FrontEnd/assets/owlcarousel/assets/owl.theme.default.min.css')}}">
-    <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
-<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 
 {{-- ------------ BackEnd Linkes ----------------- --}}
     <link rel="icon" href="images/favicon.ico" type="image/ico" />
@@ -64,7 +62,5 @@
         src="https://code.jquery.com/jquery-3.5.1.min.js"
         integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
         crossorigin="anonymous"></script>
-
-
 
   </head>
