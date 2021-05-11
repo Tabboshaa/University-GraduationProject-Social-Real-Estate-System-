04db8f60c7a3f8ea662c4e04c8a4f9be467faa0b
AlyZayed44 <61541819+AlyZayed44@users.noreply.github.com>
2021-05-11 15:22:05 +0200

    aaa

--- realEstate/resources/views/website/frontend/owner/Show_Item.blade.php
+++ realEstate/resources/views/website/frontend/owner/Show_Item.blade.php
@@ -1,35 +1,43 @@
 @extends('website.frontend.layouts.main')
 @section('content')
+<link href="{{asset('css/FrontEndCSS/MyItems.css')}}" rel="stylesheet" type="text/css">
 <div id="content-wrapper">
     <div class="container-fluid">
         <!-- Banner -->
         <div class="dashboard">
                
-                            @if(!empty($items))
-                            @foreach($items as $item)
+                           
                             
             <div class="row">
+            
 	                <div class="filterDiv Places">
+                   
                         <div class=" mylisting">
-                            
+                        @if(!empty($items))
+                            @foreach($items as $item)
                             <div class="col-md-3 col-xs-12 ">
+                            
                                 <div class="box">
-                                    <div class="box-img">
-                                        <img class="background" src="{{asset('storage/cover page/'.$item->path)}}" alt="" style="height:100pt; width:100pt;">
-                                    </div>
-                                    <div class="box-heading2" >
-                                        <h1>{{$item->Item_Name}}</h1>
-                                    </div>
+                                    
+                                        <div class="box-img">
+                                        <img class="background" src="{{asset('storage/cover page/'.$item->path)}}" alt="" style="height: 150px;">
+                                        </div>
+                                        <div class="notification-info">
+                                        <h4>{{$item->Item_Name}}</h4>
+                                    </div>   
+                                   
                                 </div>
+                            
                             </div>
-
-</div>
-</div>
-</div>
-</div>
-
                             @endforeach
-                            @endif
+                            @endif 
+                        </div>
+                        
+                    </div>
+                   
+            </div>
+
+                            
              
 
         </div>
