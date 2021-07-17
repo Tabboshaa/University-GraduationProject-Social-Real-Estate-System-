af0fa527f42821b05fffbfa735fdc78290cf5249
Abdelaziz <20173089@fue.edu.eg>
2021-07-17 21:40:22 +0200

    h

---
+++ realEstate/resources/views/emails/mailus.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>ItsolutionStuff.com</title>
+</head>
+<body>
+<h2>this mail from : </h2>
+<h2>User ID : </h2>
+<h2>User Name:</h2>
+<h3>{{$text}}</h3>
+</body>
+</html>
